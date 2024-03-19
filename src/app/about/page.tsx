f4121639bf2958ea1bbd3874f59527129de12f4a
Raffi Demirian <raffi.demirian@gmail.com>
2024-03-19 16:39:04 +1100

    about page completion

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,47 @@
 "use client"
 import Header from "@/components/header"
 import Timeline from "@/components/timeline"
-import { Box } from "@chakra-ui/react"
-import jobs from "./jobs.json"
+import { Box, Center, Stack, Text } from "@chakra-ui/react"
 
 export default function About() {
 	return (
-		<Box className="pageContents">
+		<Box className="aboutPageContents">
 			<Header showNav={true}></Header>
-			<Timeline jobs={jobs}></Timeline>
+			<Center>
+				<Stack
+					className="aboutSummary"
+					width={"95%"}
+					gap={"1em"}
+					textAlign={"center"}
+					padding={"1em"}
+					fontSize={"1em"}
+					borderColor={"black"}
+					border={"solid"}
+					borderWidth={"4px"}
+					marginTop={"15px"}
+					maxWidth={"900px"}
+					sx={{
+						"@media only screen and (min-width: 1170px)": {
+							width: "80%",
+						},
+					}}
+				>
+					<Text>
+						As an ambitious and hardworking software engineer, I am recognized
+						for my commitment, reliability, and ability to strive in all
+						environments. I am experienced in tech, operations, and project
+						management with a demonstrated history of excellence in Customer
+						Experience.
+					</Text>
+					<Text>
+						My passion for fostering cohesion and driving results makes me adept
+						at bridging the gap between technical and non-technical staff,
+						ultimately enhancing productivity and achieving organizational
+						goals.
+					</Text>
+				</Stack>
+			</Center>
+			<Timeline />
 		</Box>
 	)
 }
--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,123 +1,122 @@
-import React from "react"
-import { keyframes } from "styled-components"
-import { Box, Card, List, ListItem, Text } from "@chakra-ui/react"
-import { useColorMode } from "@chakra-ui/react"
+import React, { useState, useEffect, useRef } from "react"
+import {
+	VerticalTimeline,
+	VerticalTimelineElement,
+} from "react-vertical-timeline-component"
+import jobs from "../app/about/jobs.json"
+import "react-vertical-timeline-component/style.min.css"
+import { useColorMode, Text, List, ListItem, Stack } from "@chakra-ui/react"
+import { School } from "@mui/icons-material"
+import { Work } from "@mui/icons-material"
 
-interface Job {
-	index: number
-	year: string
-	company: string
-	title: string
-	description: string[]
-	extraTitle?: string
-	extra?: string[]
-}
+const Timeline = () => {
+	const [visibleIndexes, setVisibleIndexes] = useState<number[]>([])
+	const timelineRef = useRef<HTMLDivElement>(null)
 
-const fadeIn = keyframes`
-  from {
-    opacity: 0;
-    transform: translateY(20px);
-  }
-  to {
-    opacity: 1;
-    transform: translateY(0);
-  }
-`
+	useEffect(() => {
+		const handleScroll = () => {
+			if (timelineRef.current) {
+				const timelineRect = timelineRef.current.getBoundingClientRect()
+				const elements = timelineRef.current.querySelectorAll(
+					".vertical-timeline-element"
+				)
+				const indexes: number[] = []
 
-interface TimelineProps {
-	jobs: Job[]
-}
+				elements.forEach((el, index) => {
+					const rect = el.getBoundingClientRect()
+					const isVisible = rect.top <= window.innerHeight && rect.bottom >= 0
+					if (isVisible) {
+						indexes.push(index)
+					}
+				})
 
-const Timeline: React.FC<TimelineProps> = ({ jobs }) => {
-	const reversedJobs = [...jobs].reverse()
+				setVisibleIndexes(indexes)
+			}
+		}
+		handleScroll()
+
+		window.addEventListener("scroll", handleScroll)
+		return () => window.removeEventListener("scroll", handleScroll)
+	}, [])
+
 	const { colorMode } = useColorMode()
-	return (
-		<Box
-			display="flex"
-			alignItems="center"
-			justifyContent="center"
-			position="relative"
-			width="100%"
-		>
-			{/* Vertical Line */}
-			<Box
-				position="absolute"
-				top={0}
-				bottom={0}
-				left="50%"
-				borderLeft="1px solid #000"
-				height="100%"
-			/>
+	const reversedJobs = [...jobs].reverse()
 
-			<Box display="flex" flexDirection="column" width="50%">
+	return (
+		<div ref={timelineRef}>
+			<VerticalTimeline
+				layout="2-columns"
+				className="timeline"
+				lineColor={colorMode === "light" ? "#252425" : "#FFFADD"}
+				animate={true}
+			>
 				{reversedJobs.map((job, index) => (
-					<Box
+					<VerticalTimelineElement
 						key={index}
-						display="flex"
-						alignItems="center"
-						justifyContent={index % 2 === 0 ? "flex-start" : "flex-end"}
-						position="relative"
-						padding={2}
-						marginBottom={4}
-						width="100%"
-						marginLeft={index % 2 === 0 ? "-15%" : "15%"} // Adjusted marginLeft for better positioning
-						marginRight={index % 2 === 0 ? "20%" : "10%"} // Adjusted marginRight for better positioning
+						data-index={index}
+						dateClassName="timelineDate"
+						date={job.year}
+						visible={visibleIndexes.includes(index)}
+						icon={<Work />}
+						contentStyle={{
+							boxShadow: "none",
+							borderRadius: "15px",
+							fontSize: "12px",
+							background:
+								colorMode === "light"
+									? "rgba(0, 0, 0, 0.1)"
+									: "rgba(128, 128, 128, 0.3)",
+						}}
+						contentArrowStyle={{
+							borderRightColor:
+								colorMode === "light"
+									? "rgba(0, 0, 0, 0.1)"
+									: "rgba(128, 128, 128, 0.3)",
+						}}
+						iconStyle={{
+							color: colorMode === "light" ? "#252425" : "#FFFADD",
+							background: "orange",
+							boxShadow:
+								colorMode === "light"
+									? "#252425 0px 0px 0px 4px, rgba(0, 0, 0, 0.08) 0px 2px 0px 0px inset, rgba(0, 0, 0, 0.05) 0px 3px 0px 4px"
+									: "#FFFADD 0px 0px 0px 4px, rgba(0, 0, 0, 0.08) 0px 2px 0px 0px inset, rgba(0, 0, 0, 0.05) 0px 3px 0px 4px",
+						}}
 					>
-						{/* Circle Marker */}
-						<Box
-							position="absolute"
-							left="50%"
-							transform="translateX(-50%)"
-							borderRadius="50%"
-							width="12px"
-							height="12px"
-							backgroundColor="#000"
-						/>
-						<Card
-							padding={"1em"}
-							margin={"10px"}
-							sx={{
-								backgroundColor:
-									colorMode === "light"
-										? "rgba(0, 0, 0, 0.1)"
-										: "rgba(128, 128, 128, 0.4)",
-								borderRadius: "15px",
-								minHeight: 200,
-								color: colorMode === "light" ? "#141214" : "#FFFADD",
-								fontSize: "14px",
-								maxWidth: "400px",
-							}}
-						>
-							<Text fontWeight={"bold"} color={"orange"}>
+						<Stack direction={"column"}>
+							<Text
+								className="timelineTitleAndCompany"
+								color={"orange"}
+								fontSize={"1em"}
+							>
 								{job.title} @ {job.company}
 							</Text>
-							<Text paddingBottom={"1rem"}>{job.year}</Text>
-							<List>
-								{job.description.map((value, key) => (
-									<ListItem key={key}>
-										<Text>- {value}</Text>
-									</ListItem>
-								))}
-							</List>
 							{job.extraTitle && (
-								<Text paddingTop={"1rem"} fontWeight={"bold"}>
+								<Text className="timelineExtraTitle" fontWeight={"bold"}>
 									{job.extraTitle}
 								</Text>
 							)}
 							{job.extra && job.extra !== null && (
 								<List>
 									{job.extra.map((value, key) => (
-										<ListItem key={key}>
-											<Text>- {value}</Text>
+										<ListItem key={key}>- {value}</ListItem>
+									))}
+								</List>
+							)}
+							<Text fontSize={"12px"}>Responsibilities</Text>
+							{typeof job.description === "object" && (
+								<List>
+									{Object.entries(job.description).map(([key, value]) => (
+										<ListItem key={key} fontSize={"12px"}>
+											- {value}
 										</ListItem>
 									))}
 								</List>
 							)}
-						</Card>
-					</Box>
+						</Stack>
+					</VerticalTimelineElement>
 				))}
-			</Box>
-		</Box>
+			</VerticalTimeline>
+		</div>
 	)
 }
 
